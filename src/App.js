f6a9e3b6e98d13e7d15ad408aadf204fd9d799c3
siddharth sambharia <sam.siddharth10@gmail.com>
2023-02-14 14:03:05 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import g1 from "./images/gmail3.png";
 import s1 from "./images/slack1.png";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Friday|Berry</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
       <div className="topbar">
         <div className="berry">
-          <a href="https://useberry.me/">Berry</a>
+          <a href="https://useberry.me/">Home</a>
         </div>
         <div>
           <a href="mailto:sam.siddharth10@gmail.com">Contact Us</a>
@@ -16,7 +23,7 @@ function App() {
       <div className="heading">
         <h1>
           <p className="hover-underline-animation">
-            Experience the Power of AI in Your Daily Work Life
+            Generative AI for your Slack Workspace 🪄
           </p>
         </h1>
       </div>
@@ -27,40 +34,50 @@ function App() {
           className="form"
         >
           <label className="label">
-            For More Updates :
-            <input className="input" type="email" name="email"></input>
+            <input
+              className="input"
+              type="email"
+              name="email"
+              placeholder="Your email:"
+            ></input>
           </label>
           <button className="btn" type="submit">
-            Send
+            Sign up
           </button>
         </form>
       </div>
       <div className="hero">
         <div className="box1">
-          <div className="b11">FRIDAY</div>
-          <div className="b12">
-            Make your Slack workspace smarter and more productive with Friday.
-            <br></br>
-            AI-powered assistant that can help you with tasks like scheduling
-            meetings, organizing information, and answering questions.
-          </div>
+          <div className="sec1">
+            <div className="b11">FRIDAY</div>
+            <div className="b12">
+              Chat-GPT Slackbot: Streamline Your Business Processes and Boost
+              Productivity
+            </div>
+            <div className="b15">
+              Make your Slack workspace smarter and more productive with Friday.
+              <br></br>
+              AI-powered assistant that can help you with taskslike scheduling
+              meetings, organizing information, and answering questions.
+            </div>
 
-          <div className="b14">
-            <a href="https://slack.com/oauth/v2/authorize?client_id=4166115177239.4742632756229&scope=chat:write,im:history,mpim:history,mpim:read,mpim:write,app_mentions:read,commands,im:read,im:write,channels:history,incoming-webhook&user_scope=">
-              <img
-                alt="Add to Slack"
-                height="40"
-                width="139"
-                src="https://platform.slack-edge.com/img/add_to_slack.png"
-                srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-              />
-            </a>
+            <div className="b14">
+              <a href="https://slack.com/oauth/v2/authorize?client_id=4166115177239.4742632756229&scope=chat:write,im:history,mpim:history,mpim:read,mpim:write,app_mentions:read,commands,im:read,im:write,channels:history,incoming-webhook&user_scope=">
+                <img
+                  alt="Add to Slack"
+                  height="40"
+                  width="139"
+                  src="https://platform.slack-edge.com/img/add_to_slack.png"
+                  srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                />
+              </a>
+            </div>
           </div>
           <div className="b13">
-            <img src={s1} alt="" height="400" width="560" />
+            <img className="img" src={s1} alt="" />
           </div>
         </div>
-        <div className="box2">
+        {/* <div className="box2">
           <div className="b21">Gmail</div>
           <div className="b22">
             Take your Gmail experience to the next level with our Extension. Use
@@ -77,7 +94,7 @@ function App() {
           <div className="b23">
             <img src={g1} alt="" height="250" width="400" />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
