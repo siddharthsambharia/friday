8512d001fab7b174b764ddc6085f00939ef238ea
siddharth sambharia <sam.siddharth10@gmail.com>
2023-02-25 18:02:21 +0530

    neww

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import g1 from "./images/gmail3.png";
-import s1 from "./images/slack1.png";
 import axios from "axios";
-import slacklogo from "./images/slacklogo.png";
-// import ReactTooltip from "react-tooltip";
-import { Tooltip as ReactTooltip } from "react-tooltip";
 import { Helmet } from "react-helmet";
 
 import React, { useState } from "react";
@@ -61,25 +56,13 @@ function App() {
       "https://fridayv2-production.up.railway.app/slack/install";
   }
 
-  // const TRACKING_ID = "G-N8ZYGW08G1"; // YOUR_OWN_TRACKING_ID
-  // ReactGA.initialize(TRACKING_ID);
-
   return (
     <div className="App">
       <Helmet>
         <meta charSet="utf-8" />
         <title>Friday</title>
-        {/* <link rel="canonical" href="http://mysite.com/example" /> */}
       </Helmet>
 
-      {/* <div className="topbar">
-        <div className="berry">
-          <a href="https://useberry.me/">Home</a>
-        </div>
-        <div>
-          <a href="mailto:sam.siddharth10@gmail.com">Contact Us</a>
-        </div>
-      </div> */}
       <div className="heading">
         <h1>
           <p className="hover-underline-animation">Get Started, It's Free!</p>
@@ -108,7 +91,6 @@ function App() {
             data-for="tool"
           >
             Submit
-            {/* <img alt="Add to Slack" height="25" width="25" src={slacklogo} /> */}
           </button>
         </form>
       </div>
@@ -118,58 +100,7 @@ function App() {
           your explicit consent. Your information is secure .
         </h2>
       </div>
-      {/* <div className="email">
-        <div className="b14">
-          <a href="https://fridayv2-production.up.railway.app/slack/install">
-            <img
-              alt="Add to Slack"
-              height="40"
-              width="139"
-              src="https://platform.slack-edge.com/img/add_to_slack.png"
-              srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-            />
-          </a>
-        </div>
-      </div> */}
-
-      <div className="hero">
-        {/* <div className="box1">
-          <div className="sec1">
-            <div className="b11">friday</div>
-            <div className="b12">
-              Chat-GPT Slackbot: Streamline Your Business Processes and Boost
-              Productivity
-            </div>
-            <div className="b15">
-              Make your Slack workspace smarter and more productive with Friday.
-              <br></br>
-              AI-powered assistant that can help you with taskslike scheduling
-              meetings, organizing information, and answering questions.
-            </div>
-          </div>
-          <div className="b13">
-            <img className="img" src={s1} alt="" />
-          </div>
-        </div> */}
-        {/* <div className="box2">
-          <div className="b21">Gmail</div>
-          <div className="b22">
-            Take your Gmail experience to the next level with our Extension. Use
-            the power of AI to write eye-catching emails right inside your gmail
-            inbox.
-          </div>
-          <div className="b24">
-            <button className="btn2">
-              <a href="https://drive.google.com/drive/folders/1TNTMdsAgAUffJnScOqhF270_P9NpEyGc?usp=sharing">
-                Download
-              </a>
-            </button>
-          </div>
-          <div className="b23">
-            <img src={g1} alt="" height="250" width="400" />
-          </div>
-        </div> */}
-      </div>
+      <div className="hero"></div>
     </div>
   );
 }
